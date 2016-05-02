1b34436a3613650c41853619f120baa7cbb598c9
Fernando <ftchame@uc.cl>
2016-05-02 18:57:01 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502191812) do
-
+ActiveRecord::Schema.define(version: 20160502205247) do
 
   create_table "almacens", force: :cascade do |t|
     t.string   "_id"
@@ -33,6 +32,11 @@ ActiveRecord::Schema.define(version: 20160502191812) do
   end
 
   create_table "controladors", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "facturas", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
