0afa4d828264806b6212cdc6be20964ba735e9fa
Javier <jsespin1@uc.cl>
2016-05-03 10:44:31 -0300

    hotfix 2

--- app/controllers/almacen_controller.rb
+++ app/controllers/almacen_controller.rb
@@ -7,7 +7,6 @@ class AlmacenController < ApplicationController
 		id = "57136f3ba7aa2f03002639f4"
 		sku = 38
 		qty = 300
-		Controlador.procesar_oc(id)
 		#Request.create_orden('b2b', 4, '38', '571262b8a980ba030058ab52', '571262b8a980ba030058ab52', 1513, 3563209336999, 'jpp' )
 		#@oc = Request.getOC("57127d2e8a9e6506000b998a")
 		#Ftp.showls
