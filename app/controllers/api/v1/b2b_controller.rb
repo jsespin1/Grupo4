8f7a99f9afe1be5ece5043c75b315850934add24
Javier <jsespin1@uc.cl>
2016-05-05 10:27:32 -0300

    hotfix

--- app/controllers/api/v1/b2b_controller.rb
+++ app/controllers/api/v1/b2b_controller.rb
@@ -8,10 +8,10 @@ class Api::V1::B2bController < ApplicationController
 				id = params[:_id].to_i
 				respuesta = Controlador.getStock(params[:_id].to_i)
 				total = respuesta.to_i
-				if total > 0
+				if total
 					format.json {render json: {stock: total, sku: id},status:200}
 				else
-					format.json {render json: {description: 'No hay productos de ese SKU'},status:404}
+					format.json {render json: {description: 'No se pudo procesar la solicitud'},status:404}
 				end
 			else
 				format.json {render json: {description: 'Missing parameters'},status:400}
