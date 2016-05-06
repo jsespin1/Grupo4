266b2283c60bcf59dbf51f7b615781441e98bd72
Javier <jsespin1@uc.cl>
2016-05-06 16:36:14 -0300

    comenzando implementacion calse Abastecer que revisa niveles

--- app/models/abastecer.rb
+++ app/models/abastecer.rb
@@ -1,2 +1,28 @@
 class Abastecer < ActiveRecord::Base
+
+	##Se encarga de revisar los niveles de inventario en los almacenes
+	#(1) Para productos procesados, manda a producir según el nivel de inventario definido y la materia prima limitante.
+	#(2) Para materias primas, si están bajo, manda a producir o comprar según corresponda.
+	# => Pagar a fábrica antes de mandar a producir
+
+
+	def self.revisarProcesados
+		#Debemos revisar los niveles de SKUs 11 y 16
+
+
+	end
+
+	def self.revisarmp
+		
+
+	end
+
+
+	def self.pagarFabrica
+		
+
+	end
+
+
+
 end
