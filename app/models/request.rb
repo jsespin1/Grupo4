0d481388d828b363962fe73ad2a4653f324ae759
Javier <jsespin1@uc.cl>
2016-05-09 14:02:27 -0300

    arreglado problema credenciales

--- app/models/request.rb
+++ app/models/request.rb
@@ -67,7 +67,11 @@ class Request < ActiveRecord::Base
 	end
 
 	def  self.get_hash(parametros="")
-		 hash = Base64.encode64((HMAC::SHA1.new("tdk6NIzbhNfORDP") << parametros).digest).strip
+		if Rails.env == 'development'
+            hash = Base64.encode64((HMAC::SHA1.new("tdk6NIzbhNfORDP") << parametros).digest).strip
+        else
+            hash = Base64.encode64((HMAC::SHA1.new("W82IWMQu3Ex9YzN") << parametros).digest).strip
+        end
 		 auth_header = { 'Authorization' => "INTEGRACION grupo4:"+hash.to_s,  'Content-Type' => "application/json"}
 	end
 
