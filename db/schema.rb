090d39b4512f700e95ace68749588a5d2a0a0b26
Francisco <fagarri@uc.cl>
2016-05-18 20:18:35 +0000

    enviar orden

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507171043) do
+ActiveRecord::Schema.define(version: 20160506025406) do
 
   create_table "abastecers", force: :cascade do |t|
     t.datetime "created_at", null: false
