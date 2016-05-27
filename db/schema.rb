d6b1dc124426e3ddba68077ee250d4a4acf96699
Javier <jsespin1@uc.cl>
2016-05-27 14:48:29 -0400

    se agrego vista inicio en spree

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 20160518231535) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "compras", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "controladors", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
