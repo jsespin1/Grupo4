d18f134aac25f0de1a8629331f0fd390e40d79ba
Javier <jsespin1@uc.cl>
2016-05-27 17:03:46 -0400

    se añadio mostrar stock de productos

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -3,5 +3,11 @@ module Spree
     def inicio
       @products = Product.all
     end
+    def index
+      @stock = 0
+      @products = Product.all
+      @searcher = build_searcher(params.merge(include_images: true))
+      @taxonomies = Spree::Taxonomy.includes(root: :children)
+    end
   end
 end
