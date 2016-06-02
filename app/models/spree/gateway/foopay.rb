2bb94136139c9b7ed06010b638ed5626ecd20497
Javier <jsespin1@uc.cl>
2016-06-02 16:07:37 -0400

    añadiendo proceso de pago en processing.rb de spree y nuestras clases para redirigir

--- app/models/spree/gateway/foopay.rb
+++ app/models/spree/gateway/foopay.rb
@@ -1,3 +1,4 @@
+
 class Spree::Gateway::Foopay < Spree::Gateway
   def provider_class
     Spree::Gateway::Foopay
@@ -11,6 +12,16 @@ class Spree::Gateway::Foopay < Spree::Gateway
   end
 
   def purchase(amount, transaction_details, options = {})
+    #Aca Llamamos al método de pago INTEGRACION
+    numero_orden = options[:order_id]
+    numero_orden = numero_orden.split("-")[0]
+    orden = Spree::Order.find_by_number(numero_orden)
+    product = orden.line_items[0]
+    sku = product.sku
+    cantidad_requerida = product.quantity.to_i
+    price = product.price.to_i
+    address = options[:billing_address][:address1]
+    Boletum.crearBoleta(sku, cantidad_requerida, address, price)
     ActiveMerchant::Billing::Response.new(true, 'success', {}, {})
   end
 end
