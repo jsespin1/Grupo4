566ce8874ad08156757ea6adbe9e108361a38ff6
Javier <jsespin1@uc.cl>
2016-06-03 17:32:54 -0400

    rutas funcionando

--- app/models/boletum.rb
+++ app/models/boletum.rb
@@ -12,9 +12,8 @@ class Boletum < ActiveRecord::Base
 
     def self.getUrl(boletaId)
         if Rails.env == 'development'
-            url = "http://integracion-2016-dev.herokuapp.com/web/pagoenlinea?callbackUrl=localhost%3A3000%2Fexito&cancelUrl=localhost%3A3000%2Ffalla&boletaId="+boletaId
+            url = "http://integracion-2016-dev.herokuapp.com/web/pagoenlinea?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2Fexito&cancelUrl=http%3A%2F%2Flocalhost%3A3000%2Ffalla&boletaId="+boletaId
         else
-            #RECORDAR CAMBIARLO A PROD
             url = "http://integracion-2016-prod.herokuapp.com/web/pagoenlinea?callbackUrl=http%3A%2F%2Fintegra4.ing.puc.cl%2Fexito&cancelUrl=http%3A%2F%2Fintegra4.ing.puc.cl%2Ffalla&boletaId="+boletaId
         end
         url
