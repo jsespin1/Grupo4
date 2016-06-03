1056ca066dba759c302cd703567e2222eef5cecb
Francisco <fagarri@uc.cl>
2016-06-03 21:17:32 +0000

    falla pago y decimales iva

--- app/models/boletum.rb
+++ app/models/boletum.rb
@@ -1,6 +1,7 @@
 class Boletum < ActiveRecord::Base
     
-    def self.crearBoleta(sku, cantidad, direccion, monto)    
+    def self.crearBoleta(sku, cantidad, direccion, monto)
+        monto = monto*1.19
         boleta=Request.crear_boleta(Factura.getIdPropio, direccion, monto)
         boletafinal = Boletum.new(proveedor: Factura.getIdPropio, direccion: direccion, monto: monto.to_i, idboleta: boleta['_id'], cantidad: cantidad.to_i, sku: sku) 
         boletafinal.save
@@ -11,7 +12,7 @@ class Boletum < ActiveRecord::Base
 
     def self.getUrl(boletaId)
         if Rails.env == 'development'
-            url = "http://integracion-2016-dev.herokuapp.com/web/pagoenlinea?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2Fexito&cancelUrl=http%3A%2F%2Fwww.emol.com&boletaId="+boletaId
+            url = "http://integracion-2016-dev.herokuapp.com/web/pagoenlinea?callbackUrl=https%3A%2F%2Fgrupo4v2-fgarri.c9users.io%2Fexito&cancelUrl=https%3A%2F%2Fgrupo4v2-fgarri.c9users.io%2Ffalla&boletaId="+boletaId
         else
             #RECORDAR CAMBIARLO A PROD
             url = "http://integracion-2016-dev.herokuapp.com/web/pagoenlinea?callbackUrl=http%3A%2F%2Fintegra4.ing.puc.cl%2Fexito&cancelUrl=http%3A%2F%2Fwww.emol.com&boletaId="+boletaId
