bcf0cce00a63ae3e3612ed0c1d24c01662517ed3
Fernando <ftchame@uc.cl>
2016-06-03 13:58:05 -0300

    jaspinoz

--- db/schema.rb
+++ db/schema.rb
@@ -47,11 +47,6 @@ ActiveRecord::Schema.define(version: 20160527203150) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "compras", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "controladors", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
