b3dd19559cac2357c54bbe61eddb84cf89fd47cd
Javier <jsespin1@uc.cl>
2016-06-04 13:12:35 -0400

    arreglando problema con request y respuesta en servidor

--- app/models/boletum.rb
+++ app/models/boletum.rb
@@ -3,7 +3,7 @@ class Boletum < ActiveRecord::Base
     def self.crearBoleta(sku, cantidad, direccion, monto)
         monto = monto
         boleta=Request.crear_boleta(Factura.getIdPropio, direccion, monto)
-        boletafinal = Boletum.new(proveedor: Factura.getIdPropio, direccion: direccion, monto: monto.to_i, idboleta: boleta[0]['_id'], cantidad: cantidad.to_i, sku: sku) 
+        boletafinal = Boletum.new(proveedor: Factura.getIdPropio, direccion: direccion, monto: monto.to_i, idboleta: boleta.to_s, cantidad: cantidad.to_i, sku: sku) 
         boletafinal.save
         boletaid = boleta[0]['_id']
         url = getUrl(boletaid)
