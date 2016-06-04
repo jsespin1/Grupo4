8e82b812ed14d2a32e74cffe7f97b27ae920a3f9
Javier <jsespin1@uc.cl>
2016-06-04 12:56:17 -0400

    tratando de arreglar error desconocido

--- app/models/boletum.rb
+++ app/models/boletum.rb
@@ -3,7 +3,9 @@ class Boletum < ActiveRecord::Base
     def self.crearBoleta(sku, cantidad, direccion, monto)
         monto = monto
         boleta=Request.crear_boleta(Factura.getIdPropio, direccion, monto)
+        puts "BOLETA Request-> " + boleta.inspect
         boletafinal = Boletum.new(proveedor: Factura.getIdPropio, direccion: direccion, monto: monto.to_i, idboleta: boleta['_id'], cantidad: cantidad.to_i, sku: sku) 
+        puts "BOLETA New -> " + boletafinal.inspect
         boletafinal.save
         boletaid = boleta['_id']
         url = getUrl(boletaid)
