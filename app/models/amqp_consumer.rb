fa22f3fca4d785523bb9cea05be44cc709ec62bc
Javier <jsespin1@uc.cl>
2016-06-28 13:49:16 -0400

    lsfjklgs

--- app/models/amqp_consumer.rb
+++ app/models/amqp_consumer.rb
@@ -15,14 +15,14 @@ conn = Bunny.new(:host => "hyena.rmq.cloudamqp.com",
         :password => "moDjqWMbjzS31KhguvPnCICBEKa6V-4j",
         :automatically_recover => false
         )
-
+#conn = Bunny.new(url)
 #The connection will be established when start is called 
 conn.start 
 
 #Create a channel in the TCP connection
 ch = conn.create_channel 
 #Declare a queue with a given name, examplequeue. In this example is a durable shared queue used.
-q  = ch.queue("ofertas", :durable => true)
+q  = ch.queue("ofertas", :auto_delete => true)
 
 
 #Set up the consumer to subscribe from the queue
@@ -32,12 +32,11 @@ q.subscribe(:block => true, :manual_ack => true) do |delivery_info, properties, 
   puts "Json info: " << json_information_message.inspect
   #if Rails.env == 'development'
   	#nack(delivery_tag, multiple = false, requeue = false)
-  	ch.basic_nack(delivery_info.delivery_tag, true, false)
+  	ch.basic_nack(delivery_info.delivery_tag, true, true)
   #end
   #ch.ack(delivery_info.delivery_tag, true)
   #ch.consumers[delivery_info.consumer_tag].cancel
-  puts "delivery_info: " << delivery_info.inspect
-  puts "Properties: " << properties.inspect
+  puts "JSON: " << json_information_message['sku']
   #mostrar(json_information_message)
 end
 
