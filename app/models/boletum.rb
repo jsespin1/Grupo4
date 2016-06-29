817e94eb8a52f39e3843297e81589e02bd4faf9d
Javier <jsespin1@uc.cl>
2016-06-29 17:46:30 -0400

    arreglando errores de rutas spree

--- app/models/boletum.rb
+++ app/models/boletum.rb
@@ -13,9 +13,9 @@ class Boletum < ActiveRecord::Base
     def self.getUrl(boletaId)
         #"http://integracion-2016-dev.herokuapp.com/web/pagoenlinea?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2Fexito&cancelUrl=http%3A%2F%2Flocalhost%3A3000%2Ffalla&boletaId="+boletaId
         if Rails.env == 'development'
-            url = "http://integracion-2016-dev.herokuapp.com/web/pagoenlinea?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2Fexito&cancelUrl=http%3A%2F%2Flocalhost%3A3000%2Ffalla&boletaId="+boletaId
+            url = "http://integracion-2016-dev.herokuapp.com/web/pagoenlinea?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2Fspree%2Fexito&cancelUrl=http%3A%2F%2Flocalhost%3A3000%2Fspree%2Ffalla&boletaId="+boletaId
         else
-            url = "http://integracion-2016-prod.herokuapp.com/web/pagoenlinea?callbackUrl=http%3A%2F%2Fintegra4.ing.puc.cl%2Fexito&cancelUrl=http%3A%2F%2Fintegra4.ing.puc.cl%2Ffalla&boletaId="+boletaId
+            url = "http://integracion-2016-prod.herokuapp.com/web/pagoenlinea?callbackUrl=http%3A%2F%2Fintegra4.ing.puc.cl%2Fspree%2Fexito&cancelUrl=http%3A%2F%2Fintegra4.ing.puc.cl%2Fspree%2Ffalla&boletaId="+boletaId
         end
         url
     end
